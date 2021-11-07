7fd5c8afb0470b0de6df46b6fe2baff5b5afe9c0
alam3510 <alams3510@gmail.com>
2021-11-07 20:10:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import react, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  function App() {
+    const [hori, sethori] = useState(10);
+    const [veri, setveri] = useState(10);
+    const [blur, setblur] = useState(10);
+    const [color, setcolor] = useState('black');
+  
+    return (
+      <>
+        <h1>This is a Box Shadow template</h1>
+  
+          {/* creating a control section of inputs */}
+        <div className='App'>
+          <div className='controls'>
+            <label>Horizontal :</label>
+            <input type='range' min='-100' max='100' value={hori} onChange={(e) => sethori(e.target.value)} />
+            <label>Vertical :</label>
+            <input type='range' min='-100' max='100' value={veri} onChange={(e) => setveri(e.target.value)} />
+            <label>Blur :</label>
+            <input type='range' min='0' max='100' value={blur} onChange={(e) => setblur(e.target.value)} />
+            <label>color :</label>
+            <input type='color' value={color} onChange={(e) => setcolor(e.target.value)} />
+          </div>
+  
+  {/* applyying the useset and displaying */}
+          <div className='box' style={{ boxShadow: `${hori}px ${veri}px ${blur}px ${color}` }}>
+            <h4>Copy and paste in your code</h4>
+  
+            <h2 className='innertext'>box-shadow:{`${hori}px ${veri}px ${blur}px`}</h2>
+            <h3>color:{`${color}`}</h3>
+          </div>
+        </div>
+        <div className='footer'>
+          <h3>Created by Shahbaz alam</h3>
+        </div>
+      </>
+   
+    
+  )
 }
 
 export default App;
